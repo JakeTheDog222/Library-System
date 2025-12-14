3ff2d8f561241b6d91bcbeb6457c70e16dbfe56e
JakeTheDog222 <angelobadi124@gmail.com>
2025-12-15 00:26:36 +0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header class="no-left-radius">
+    <h1><img src="../image/WMSU.png" alt="WMSU Logo" style="border-radius: 50%;">Library Book Borrowing System</h1>
+    <nav>
+        <ul>
+            <li>Welcome, Admin!</li>
+            <li><a href="../logout.php" class="logout">Logout</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ includes/sidebar.php
@@ -0,0 +1,12 @@
+<aside class="sidebar">
+    <h2>Admin Menu</h2>
+    <ul>
+        <li><a href="dashboard.php"><i class="fas fa-tachometer-alt"></i>Dashboard</a></li>
+        <li><a href="books.php"><i class="fas fa-book"></i>Manage Books</a></li>
+        <li><a href="borrow_history.php"><i class="fas fa-history"></i>Borrow History</a></li>
+        <li><a href="pending_requests.php"><i class="fas fa-clock"></i>Pending Requests</a></li>
+        <li><a href="fines.php"><i class="fas fa-money-bill-wave"></i>Fines Management</a></li>
+        <li><a href="genres.php"><i class="fas fa-tags"></i>Genres</a></li>
+        <li><a href="students.php"><i class="fas fa-users"></i>List of Account</a></li>
+    </ul>
+</aside>
